28086bef3efe1fe3c3a910a28e2cce48d9fa43bf
marszysz <marszysz@gmail.com>
2016-08-22 23:31:07 +0200

    Working async getExifDate

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 'use strict';
+const fs = require ('fs');
 
 function listFiles (dir) {
-    return null;
+    return fs.readdirSync (dir);
 }
 
+function getExifDate (fileName, callback) {
+    var fs = require('fs');
+    fs.stat(fileName, function(error, stats) {
+        // todo: make some idiot-proofing at opening file (file accesibility etc.)
+        // the best method for an error seems to be null result and a notice to the console 
+        fs.open(fileName, 'r', function(error, fileDescriptor) {
+            var exifEnd = 65635; // the maximum position within the JPEG file where EXIF metadata block could reach
+            var exifBuffer = Buffer.allocUnsafe(stats.size < exifEnd ? stats.size : exifEnd);
+            fs.read(fileDescriptor, exifBuffer, 0, exifBuffer.length, 0, function(error, bytesRead, buffer) {
+                fs.close(fileDescriptor, (error) => console.log('Done with a file: ', fileName));
+                var parser = require('exif-parser').create(buffer);
+                parser.enableImageSize(false);
+                var result = parser.parse();
+                callback(result.tags.DateTimeOriginal);
+            });
+        });
+    });
+}
+
+// exports for the unit tests
 exports.listFiles = listFiles;
+exports.getExifDate = getExifDate;
--- test.js
+++ test.js
@@ -3,11 +3,18 @@ import test from 'ava';
 // czy nie ma jakiejś sztuczki typu include, żeby nie trzeba było pisać `export` przed każdą funkcją?
 // jakieś coś, co by wrzucało treść modułu, nawet bezpośrednio luzem, w zasięg bieżącego skryptu...
 // nie działa: import {listFiles} from './main'; // może dlatego nie działa, że był jakiś problem z `export`?...
-var main = require('./main');
+const main = require('./main');
 
 // trochę głupawy test, ale o trzeciej nad ranem nie miałem lepszego pomysłu
 test('listFiles should return proper file list', t => {
-    t.is(main.listFiles('testdir'), ['1.jpg', 'a.jpg', 'b.jpg']);
+    t.deepEqual(main.listFiles('testdir'), ['1.jpg', 'a.jpg', 'b.jpg']);
+}); 
+
+// getEXIFdate
+test('getExifDate should return `date taken` of a jpg file', t => {
+    main.getExifDate('testdir/1.jpg', (date) => {
+        t.deepEqual(date, new Date(2011, 6, 1, 7, 7, 7))
+    });
 });
 
 // test.todo ('write some tests');
