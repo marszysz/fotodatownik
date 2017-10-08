4fc21840dc67cb10be4e0daef614b6b6f04f2fc8
marcin <marszysz@gmail.com>
2017-10-09 01:04:43 +0200

    Confirmation dialogue window got usable styling

--- app/conf_dialog.js
+++ app/conf_dialog.js
@@ -4,12 +4,12 @@ var ipc = require('electron').ipcRenderer;
 
 ipc.once('passData', makeTable);
 function makeTable (event, data) {
-    var table = document.getElementById('changeList');
+    var tableBody = document.getElementById('changeList');
     Object.keys(data).forEach(src => {
         let dest = data[src] ? data[src] : 'Bez zmiany - data nieznana';
         let row = document.createElement('tr');
         row.innerHTML = `<td>${src}</td><td>${dest}</td>`;
-        table.appendChild(row);
+        tableBody.appendChild(row);
     });
     ['ok', 'cancel'].forEach(msg => {
         document.getElementById(msg).addEventListener('click', () => event.sender.send('resp', msg));
