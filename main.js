3fe92afa6c3b0eae6e7ecdbaebb0722dcdbe8bce
marcin <marszysz@gmail.com>
2017-10-09 01:10:02 +0200

    TODO tweak

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ app.on('ready', () => {
 	mainWindow = createMainWindow();
 });
 
-// TODO: use Photon
+// TODO: use Photon or Quasar or CSS Grid Layout (at a pinch)
 // TODO: find about possibility of cross-platform shell integration
 
 // TODO: undo history stored in settings?
