fb90b8f66c93c9f228e47506de71719c451dc6a0
marcin <marszysz@gmail.com>
2017-10-09 19:58:38 +0200

    Tweaks to confirmation dialog strings; added htmlspecialchars package

--- app/conf_dialog.js
+++ app/conf_dialog.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var ipc = require('electron').ipcRenderer;
+const ipc = require('electron').ipcRenderer;
+const htmlspecialchars = require('htmlspecialchars');
 
 ipc.once('passData', makeTable);
 function makeTable (event, data) {
     var tableBody = document.getElementById('changeList');
     Object.keys(data).forEach(src => {
-        let dest = data[src] ? data[src] : 'Bez zmiany - data nieznana';
+        let dest = data[src] ?
+            htmlspecialchars(data[src]) :
+            '<span class="special">Bez zmiany - data nieznana</span>';
         let row = document.createElement('tr');
-        row.innerHTML = `<td>${src}</td><td>${dest}</td>`;
+        row.innerHTML = `<td>${htmlspecialchars(src)}</td><td>${dest}</td>`;
         tableBody.appendChild(row);
     });
     ['ok', 'cancel'].forEach(msg => {
